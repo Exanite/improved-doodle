f289fbb89a2dcc9e04e8d66cc1477ed97b9f5a99
Exanite <exanite78012@gmail.com>
2021-07-22 22:24:20 -0400

    Add basic functionality

---
+++ Assets/Exanite.ImprovedDoodle/DoodleCanvas.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoodleCanvas : MonoBehaviour
+{
+    public new Camera camera;
+    public RawImage display;
+
+    private RenderTexture captureTexture;
+    private RenderTexture displayTexture;
+
+    private Vector2Int currentResolution;
+
+    public Plane Plane => new Plane(transform.forward, transform.position);
+
+    private void Start()
+    {
+        var resolution = GetScreenResolution();
+
+        UpdateRenderTextureResolution(resolution);
+
+        camera.enabled = false;
+        camera.targetTexture = captureTexture;
+    }
+
+    private void Update()
+    {
+        var resolution = GetScreenResolution();
+        if (currentResolution != resolution)
+        {
+            UpdateRenderTextureResolution(resolution);
+        }
+    }
+
+    public void Paint(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        var brush = Instantiate(prefab, position, rotation, transform);
+
+        camera.Render();
+        Graphics.Blit(captureTexture, displayTexture);
+
+        Destroy(brush);
+    }
+
+    private Vector2Int GetScreenResolution()
+    {
+        return new Vector2Int(Screen.width, Screen.height);
+    }
+
+    private void UpdateRenderTextureResolution(Vector2Int resolution)
+    {
+        currentResolution = resolution;
+
+        var options = new RenderTextureDescriptor(resolution.x, resolution.y);
+
+        captureTexture = new RenderTexture(options);
+        displayTexture = new RenderTexture(options);
+
+        display.texture = displayTexture;
+    }
+}
---
+++ Assets/Exanite.ImprovedDoodle/Painter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Exanite.ImprovedDoodle
+{
+    public class Painter : MonoBehaviour
+    {
+        public GameObject brushPrefab;
+
+        public new Camera camera;
+        public DoodleCanvas doodleCanvas;
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                var ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (doodleCanvas.Plane.Raycast(ray, out var distance))
+                {
+                    var position = ray.GetPoint(distance);
+                    var rotation = Random.rotation;
+
+                    doodleCanvas.Paint(brushPrefab, position, rotation);
+                }
+            }
+        }
+    }
+}
